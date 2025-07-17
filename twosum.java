6ab832de7a916403a0340120170f06390547d6bf
Syed Mohd Musa <syedmohdmusa2002@gmail.com>
2025-07-18 01:51:12 +0530

    Initial commit

---
+++ twosum.java
@@ -0,0 +1,14 @@
+package Java_Programs;
+
+public class twosum {
+    public static int[] findTwoSum(int[] numbers, int target) {
+        for (int i = 0; i < numbers.length; i++) {
+            for (int j = i + 1; j < numbers.length; j++) {
+                if (numbers[i] + numbers[j] == target) {
+                    return new int[] { i, j };
+                }
+            }
+        }
+        return null; // No solution found
+    }
+}
